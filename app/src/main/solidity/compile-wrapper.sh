a07d0256dc7f8c0b2651e07271934c43cb7af070
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-08-12 21:36:16 +0700

    Add ERC-721 starter

--- app/src/main/solidity/compile-wrapper.sh
+++ app/src/main/solidity/compile-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-solc HelloWorld.sol --bin --abi --optimize -o ./
-web3j generate solidity -a HelloWorld.abi -b HelloWorld.bin -o ../java --package=com.linh.web3jsample.data.contract
+solc ERC721.sol --bin --abi --optimize -o ./
+web3j generate solidity -a ERC721.abi -b ERC721.bin -o ../java --package=com.linh.web3jsample.data.contract
