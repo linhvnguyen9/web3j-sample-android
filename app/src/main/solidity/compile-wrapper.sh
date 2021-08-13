e67aabde1d62aba49f193e245c88c493a2a5bfe9
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-08-13 21:52:45 +0700

    Add ERC721 extensions

--- app/src/main/solidity/compile-wrapper.sh
+++ app/src/main/solidity/compile-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-solc ERC721.sol --bin --abi --optimize -o ./
-web3j generate solidity -a ERC721.abi -b ERC721.bin -o ../java --package=com.linh.web3jsample.data.contract
+solc NonFungibleToken4.sol --bin --abi --optimize -o ./
+web3j generate solidity -a NonFungibleToken4.abi -b NonFungibleToken4.bin -o ../java --package=com.linh.web3jsample.data.contract
