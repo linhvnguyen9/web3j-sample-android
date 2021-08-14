58f9b443335959e72f0b486aa0517d7256950ac1
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-08-14 19:59:20 +0700

    Update smart contract

--- app/src/main/java/com/linh/web3jsample/data/contract/SmartContractService.kt
+++ app/src/main/java/com/linh/web3jsample/data/contract/SmartContractService.kt
@@ -3,7 +3,7 @@ package com.linh.web3jsample.data.contract
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class SmartContractService(private val smartContract: HelloWorld) {
+class SmartContractService(private val smartContract: NonFungibleToken4) {
     fun getContractAddress() : String {
         return smartContract.contractAddress
     }
